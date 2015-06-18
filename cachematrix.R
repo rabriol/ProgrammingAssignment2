d640fdd5e7d472146eab0d648dc43eeeb55fe8f3
cad_rboliveira <cad_rboliveira@uolinc.com>
2015-06-18 08:10:00 -0300

    editing file to include my solution for cached inversed matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversedM <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                inversedM <<- NULL
+        }
+        
+        get <- function() x
+        
+        setInversed <- function(inversedM) inversedM <<- inversedM
+        
+        getInversed <- function() inversedM
+        
+        list(set = set, get = get, setInversed = setInversed, getInversed = getInversed)
 }
 
 
@@ -12,4 +25,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverserd <- x$getInversed()
+        if (!is.null(inverserd)) {
+                message("getting cached data")
+                return(inverserd)
+        }
+        
+        matrix <- x$get()
+        
+        inversed <- solve(matrix)
+        
+        x$setInversed(inversed)
+        
+        inversed
 }
